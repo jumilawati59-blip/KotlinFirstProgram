bb90b68dce50e2a8a75c819edc4d407e0bc38ccb
jumilawati59-blip <jumilawati59@gmail.com>
2025-09-06 11:21:50 +0700

    Add files via upload

---
+++ Literal.kt
@@ -0,0 +1,13 @@
+fun main (args: Array<String>) {
+    var intliteral = 5
+    var doubleliteral = .02
+    var stringliteral = "Hello"
+    var charliteral = '1'
+    var booliteral = true
+    println("intLiteral = " + intliteral)
+    println("doubleliteral = " + doubleliteral)
+    println("stringLiteral = " + stringliteral)
+    println("charLiteral = " + charliteral)
+    println("boolliteral = " + booliteral)
+
+}
